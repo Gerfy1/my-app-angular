cc06fc54fe9842477f89c1a83166a93dcc135d51
Gerfy1 <gerfynlol@gmail.com>
2025-01-23 15:54:01 -0300

    [update] start

---
+++ src/app/_components/exemplo-componente/exemplo-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-componente',
+  standalone: false,
+  
+  templateUrl: './exemplo-componente.component.html',
+  styleUrl: './exemplo-componente.component.css'
+})
+export class ExemploComponenteComponent {
+
+}

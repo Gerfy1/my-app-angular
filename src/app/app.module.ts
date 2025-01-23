249f265cc21342b8d2a996a89d17c36dbf33dc17
Gerfy1 <gerfynlol@gmail.com>
2025-01-23 16:51:08 -0300

    [update] del example, create nav/footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ExemploComponenteComponent } from './_components/exemplo-componente/exemplo-componente.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ExemploComponenteComponent
+    ExemploComponenteComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

ae6b5ef7e2c1bef5fd47013fbe29253da083e3e3
Gerfy1 <gerfynlol@gmail.com>
2025-01-30 19:44:36 -0300

    [update] add ngfor

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-users',
   standalone: false,
-  
+
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+  users: String[] = ['Fernando', 'Ricardo', 'Lucas', 'Pedro', 'Marina'];
 
 }

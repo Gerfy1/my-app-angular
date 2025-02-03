62b7e7a12c1c60ac7c202103faaa158eb4323f37
Gerfy1 <gerfynlol@gmail.com>
2025-02-03 20:19:09 -0300

    add: formGroup

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from '../../_models/user';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -12,6 +13,9 @@ export class UsersComponent {
   //users: String[] = ['Fernando', 'Ricardo', 'Lucas', 'Pedro', 'Marina'];
 
   userSelecionado: User | undefined;
+  userForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder ){}
 
   users: User [] = [
     {

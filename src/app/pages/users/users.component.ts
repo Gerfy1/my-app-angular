cbc1b8917b922644513c87c211f27812533855ed
Gerfy1 <gerfynlol@gmail.com>
2025-02-03 20:43:54 -0300

    update: reactive forms

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../_models/user';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -9,15 +9,31 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
   //users: String[] = ['Fernando', 'Ricardo', 'Lucas', 'Pedro', 'Marina'];
 
   userSelecionado: User | undefined;
   userForm: FormGroup = new FormGroup({});
 
-  constructor(private fb: FormBuilder ){}
+  constructor(private fb: FormBuilder) { }
 
-  users: User [] = [
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.userForm = this.fb.group({
+      nome: ['', [Validators.required, Validators.maxLength(250)]],
+      idade: ['', [Validators.required, Validators.min(12), Validators.max(120)]]
+    })
+  }
+  submitForm() {
+    if (this.userForm.valid){
+      console.log(this.userForm.value);
+    }
+  }
+
+  users: User[] = [
     {
       nome: 'Fernando',
       idade: 25
@@ -36,7 +52,7 @@ export class UsersComponent {
     }
   ]
 
-  infoUserSelecionado(user: User){
+  infoUserSelecionado(user: User) {
     this.userSelecionado = user;
   }
 }
